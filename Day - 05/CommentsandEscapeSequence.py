6817fb7a80aee4512dc77fbb8bca92f4901b09a2
Akshitbansal26367 <akshitbansal08082003@gmail.com>
2024-05-18 23:45:38 +0530

    Added Day - 05

---
+++ Day - 05/CommentsandEscapeSequence.py
@@ -0,0 +1,10 @@
+print("Hey I am a good boy\nand this viewer is also a good boy")
+# I am a single line comment
+'''
+I am a multiline comment
+author - akshit
+course - 100daysofcode
+'''
+print("My name is \"Akshit\"")
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Akshit")
