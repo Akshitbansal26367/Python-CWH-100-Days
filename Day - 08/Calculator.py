dcbfc2dd55904e0dee3a5e61ff39b4bc729b2cb0
Akshitbansal26367 <akshitbansal08082003@gmail.com>
2024-05-19 17:57:14 +0530

    Added Day - 08

---
+++ Day - 08/Calculator.py
@@ -0,0 +1,7 @@
+a = 56
+b = 3
+
+print("Sum of", a, "and", b, "is", a + b)
+print("Difference of", a, "and", b, "is", a - b)
+print("Multiplication of", a, "and", b, "is", a * b)
+print("Division of", a, "and", b, "is", a / b)
