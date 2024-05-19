6adec4672bec3825d48ae0008c752604262835ee
Akshitbansal26367 <akshitbansal08082003@gmail.com>
2024-05-19 13:23:03 +0530

    Added Day - 06

---
+++ Day - 06/Variables.py
@@ -0,0 +1,22 @@
+a = complex(8, 2)
+b = True
+c = "Harry"
+d = None
+print("Value of a is", a)
+print("Value of b is", b)
+print("Value of c is", c)
+print("Value of d is", d)
+print("\n")
+print("Type of a is", type(a))
+print("Type of b is", type(b))
+print("Type of c is", type(c))
+print("Type of d is", type(d))
+
+list1 = [8, 2.3, [4, -5], ["apple", "banana"]]
+print("List is ", list1)
+
+tuple1 = (("parrot", "sparrow"), "Lion", "Tiger")
+print("Tuple is ", tuple1)
+
+dict1 = {"name": "Sakshi", "age": 20, "canVote": 20}
+print("Dictionary is ", dict1)
