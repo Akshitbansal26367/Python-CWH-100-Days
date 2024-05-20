3ab9ae0011c1d2d0ca57ce446e1a1072fc0ac8da
Akshitbansal26367 <akshitbansal08082003@gmail.com>
2024-05-20 21:48:58 +0530

    Added Day - 09

---
+++ Day - 09/Typecasting.py
@@ -0,0 +1,17 @@
+a = "1"
+b = "2"
+print("Concatenation of string : ", a + b)
+
+# Explicit TypeCasting --> Done by User
+print("Sum of numbers a and b  : ", int(a) + int(b))
+
+string = "15"
+number = 7
+string_number = int(string)
+suma = number + string_number
+print("Sum is ", suma)
+
+# Implicit TypeCasting --> Done by python compiler
+c = 1.9
+d = 8
+print("Sum of c and d is ", c + d)
