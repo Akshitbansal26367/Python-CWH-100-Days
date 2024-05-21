ab9f9704fb30f6309e8d717cf636473055c83024
Akshitbansal26367 <akshitbansal08082003@gmail.com>
2024-05-21 15:08:36 +0530

    Added Day - 10

---
+++ Day - 10/UserInput.py
@@ -0,0 +1,10 @@
+a = input("Enter your name : ")
+print("My name is ", a)
+
+x = input("Enter first number  : ")
+y = input("Enter second number : ")
+print("Concatenation of string is ", x + y)
+print("Sum is ", int(x) + int(y))
+
+z = int(input("Enter z : "))
+print("z is ", z)
