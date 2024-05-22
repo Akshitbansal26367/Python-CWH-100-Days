4e1ef57a681527d643abcd0716ea92b494561c3d
Akshitbansal26367 <akshitbansal08082003@gmail.com>
2024-05-22 12:56:01 +0530

    Added 2 files

---
+++ InputOutputPractice/AllArithmeticOperations.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+
+print("\na + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", int(a / b))
+print("a % b = ", a % b)
+print("a // b = ", a // b)
---
+++ InputOutputPractice/calculateAverage.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+c = int(input("Enter c : "))
+print("Average of three numbers is %.2f" % ((a + b + c) / 3))
