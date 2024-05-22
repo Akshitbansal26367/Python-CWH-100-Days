9c04c1f8f252b9d5c3bee6cab4cf0fb692e32896
Akshitbansal26367 <akshitbansal08082003@gmail.com>
2024-05-22 13:52:22 +0530

    Added Day - 11

---
+++ Day - 11/Strings.py
@@ -0,0 +1,20 @@
+name = "Akshit"
+print("Name is", name)
+print("Name is " + name)
+
+apple = 'He said, "I want to eat an apple"'
+print(apple)
+
+# hello = '''He said
+# hey I am good
+# I want to eat an apple'''
+# print(hello)
+print("Character at index 0 is " + name[0])
+print("Character at index 1 is " + name[1])
+print("Character at index 2 is " + name[2])
+print("Character at index 3 is " + name[3])
+print("Character at index 4 is " + name[4])
+
+print("\nUsing For Loop")
+for character in apple:
+    print("Printing Using For Loops : " + character)
